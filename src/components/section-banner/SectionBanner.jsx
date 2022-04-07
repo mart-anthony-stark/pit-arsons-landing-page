1101a1e17999a790fe9a8ece4d8107a08dce68a6
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-07 20:20:25 +0800

    Add SectionBanner component

---
+++ src/components/section-banner/SectionBanner.jsx
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+const SectionBanner = ({ data }) => {
+  const { subHeading, topHeading, img } = data;
+  return (
+    <div
+      className="section-banner"
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${img})`,
+        backgroundSize: "cover",
+      }}
+    >
+      {/* <div className="overlay"></div>
+      <img src={img} /> */}
+      <h2>{topHeading}</h2>
+      <h1>{subHeading}</h1>
+    </div>
+  );
+};
+
+export default SectionBanner;
