c779c20e6d643c33cd605e294f18000645f5d9cf
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-08 00:45:56 +0800

    Add navbar cart

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import { Link } from "react-router-dom";
 import logo from "../../images/logo.png";
+import { Badge } from "@material-ui/core";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [quantity, setQuantity] = useState(1);
   return (
     <nav>
       <img className="logo" src={logo} alt="" />
@@ -13,7 +17,11 @@ const Navbar = () => {
         <Link to="/faqs">FAQs</Link>
         <Link to="/contact">Contact Us</Link>
       </div>
-      <div className="cart"></div>
+      <div className="cart">
+        <Badge badgeContent={quantity} color="red">
+          <ShoppingCartRoundedIcon />
+        </Badge>
+      </div>
     </nav>
   );
 };
