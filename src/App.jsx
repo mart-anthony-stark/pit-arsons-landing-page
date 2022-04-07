a65a78fcbd353ef39d4ff0319eb297cc0574389b
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-07 21:54:10 +0800

    Add meat route by category

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
             }
           />
           <Route
-            path="/meats"
+            path="/meats/:category"
             element={
               <Suspense fallback={<></>}>
                 <Meats />
