8995eb35d304123a7bebbb34c9966c4ed80bcc9a
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-09 16:00:38 +0800

    Add Pick-Up Address

--- src/pages/checkout/information/Collect.jsx
+++ src/pages/checkout/information/Collect.jsx
@@ -1,4 +1,18 @@
 const Collect = () => {
-  return <></>;
+  return (
+    <>
+      <h1>Pick-Up Address</h1>
+      <div className="address">
+        <div className="left">
+          11 Emerald St. Severina Diamond,
+          <br /> Paranaque City
+        </div>
+        <div>
+          <h3>FREE</h3>
+          <span>Collect between 9AM to 7PM</span>
+        </div>
+      </div>
+    </>
+  );
 };
 export default Collect;
