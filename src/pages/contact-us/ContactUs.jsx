122e0f4401cf4b372db388dc656615ae20885eae
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-09 22:21:36 +0800

    Add API integration for contact us

--- src/pages/contact-us/ContactUs.jsx
+++ src/pages/contact-us/ContactUs.jsx
@@ -4,26 +4,65 @@ import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
 import LocalPhoneRoundedIcon from "@mui/icons-material/LocalPhoneRounded";
 import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import { useRef } from "react";
+import validator from "validator";
+import toast from "react-hot-toast";
 
 const ContactUs = () => {
+  const fnameRef = useRef();
+  const lnameRef = useRef();
+  const emailRef = useRef();
+  const msgRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const firstname = fnameRef.current.value;
+    const lastname = lnameRef.current.value;
+    const email = emailRef.current.value;
+    const message = msgRef.current.value;
+
+    if (
+      validator.isEmpty(firstname) ||
+      validator.isEmpty(lastname) ||
+      validator.isEmpty(email) ||
+      validator.isEmpty(message)
+    )
+      return toast.error("All fields are required");
+
+    if (!validator.isEmail(email)) return toast.error("Invalid email");
+
+    const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/feedback`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, firstname, lastname, message }),
+    });
+    const data = await res.json();
+
+    toast.success("Feedback was submitted");
+    fnameRef.current.value = "";
+    lnameRef.current.value = "";
+    emailRef.current.value = "";
+    msgRef.current.value = "";
+  };
+
   return (
     <div className="contact-us">
       <SectionBanner
         data={{
           topHeading: "PENNY FOR YOUR THOUGHTS",
           subHeading: "CONTACT US",
-          img: 'contact',
+          img: "contact",
         }}
       />
       <section className="center col">
         <h1>We'd love to hear from you!</h1>
-        <form className="center col">
+        <form onSubmit={handleSubmit} className="center col">
           <div className="two-cols">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
+            <input type="text" placeholder="First Name" ref={fnameRef} />
+            <input type="text" placeholder="Last Name" ref={lnameRef} />
           </div>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+          <input type="text" placeholder="Email" ref={emailRef} />
+          <textarea placeholder="Message" ref={msgRef}></textarea>
           <input type="submit" value="Submit" />
         </form>
 
