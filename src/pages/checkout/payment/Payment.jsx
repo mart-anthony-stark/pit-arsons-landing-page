4bf1bc749eda1eb7d2b2237c5e1825fc450b504f
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-10 22:39:41 +0800

    Fixed asset image filename import

--- src/pages/checkout/payment/Payment.jsx
+++ src/pages/checkout/payment/Payment.jsx
@@ -1,5 +1,5 @@
 import Gcash from "../../../images/Bank Logos/gcash-logo.png";
-import COD from "../../../images/Bank Logos/cod.png";
+import COD from "../../../images/Bank Logos/CODE.png";
 import BPI from "../../../images/Bank Logos/bpi-logo.jpg";
 import BDO from "../../../images/Bank Logos/bdo-logo.png";
 import Button from "../../../components/button/Button";
