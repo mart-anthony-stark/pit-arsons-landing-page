225c85a38daa5137950ff1d7b55939d787b2e918
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-10 21:09:58 +0800

    Add cart reducer actions

--- src/redux/cartRedux.js
+++ src/redux/cartRedux.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import MeatData from "../data/meats";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -7,10 +8,40 @@ const cartSlice = createSlice({
   },
   reducers: {
     addProduct: (state, action) => {
-      state.products.push(action.payload);
+      const exist = state.products.find(
+        (p) => p.item.id == action.payload.item.id
+      );
+      if (exist) return;
+
+      state.products = [...state.products, action.payload];
+    },
+    removeProduct: (state, action) => {
+      state.products = state.products.filter(
+        (p) => p.item.id !== action.payload
+      );
+    },
+    clearItems: (state, action) => {
+      state.products = [];
+    },
+    addQuantity: (state, action) => {
+      state.products.forEach((p, i) => {
+        if (p.item.id == action.payload) state.products[i].quantity += 1;
+      });
+    },
+    decreaseQuantity: (state, action) => {
+      state.products.forEach((p, i) => {
+        if (p.item.id == action.payload && p.quantity > 1)
+          state.products[i].quantity -= 1;
+      });
     },
   },
 });
 
-export const { addProduct } = cartSlice.actions;
+export const {
+  addProduct,
+  removeProduct,
+  clearItems,
+  addQuantity,
+  decreaseQuantity,
+} = cartSlice.actions;
 export default cartSlice.reducer;
