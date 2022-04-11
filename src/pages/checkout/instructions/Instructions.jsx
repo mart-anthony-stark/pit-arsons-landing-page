f08c995bafe7c795fe93f11a3c27547ad0a97d4d
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-11 20:36:07 +0800

    Add instructions mutation

--- src/pages/checkout/instructions/Instructions.jsx
+++ src/pages/checkout/instructions/Instructions.jsx
@@ -1,12 +1,21 @@
 import Button from "../../../components/button/Button";
 import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { setInstructions } from "../../../redux/info";
 
 const Instructions = () => {
+  const dispatch = useDispatch();
+  const instructions = useSelector((state) => state.information.instructions);
+
   return (
     <div className="special-instructions">
       <h1>Special Instructions</h1>
       <div className="text-container">
-        <textarea placeholder="Leave us a note..."></textarea>
+        <textarea
+          placeholder="Leave us a note..."
+          value={instructions}
+          onChange={(e) => dispatch(setInstructions(e.target.value))}
+        ></textarea>
       </div>
       <div className="buttons flex-end center">
         <Link to="/cart">Return to Checkout</Link>
