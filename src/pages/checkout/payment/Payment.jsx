4a9db3daaba861d6edb1fd4826ed8211b50baae7
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-12 20:26:58 +0800

    Add review order ui

--- src/pages/checkout/payment/Payment.jsx
+++ src/pages/checkout/payment/Payment.jsx
@@ -5,10 +5,20 @@ import BDO from "../../../images/Bank Logos/bdo-logo.png";
 import Button from "../../../components/button/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { setPaymentMethod } from "../../../redux/info";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Payment = () => {
+  const [disabledChangeContact, setDisabledChangeContact] = useState(true);
   const dispatch = useDispatch();
   const paymentMethod = useSelector((state) => state.information.paymentMethod);
+  const cart = useSelector((state) => state.cart.products);
+  const deliveryMethod = useSelector(
+    (state) => state.information.deliveryMethod
+  );
+  const mobileContact = useSelector(
+    (state) => state.information.customer.mobile
+  );
 
   const handleClickPayment = (method) => {
     dispatch(setPaymentMethod(method));
@@ -65,6 +75,64 @@ const Payment = () => {
             {paymentMethod === "cod" ? "Selected" : "Select Payment Option"}
           </Button>
         </div>
+      </div>
+
+      <div className="review-order">
+        <h1 className="text-center">Review your order below:</h1>
+        <table className="">
+          <tbody>
+            {cart.map((product) => (
+              <tr key={product.item.id}>
+                <td>
+                  <h1>{product.quantity}x</h1>
+                </td>
+                <td>{product.item.name}</td>
+                <td>₱ {product.quantity * product.item.price}</td>
+              </tr>
+            ))}
+            <tr className="sub-total">
+              <td></td>
+              <td>Subtotal:</td>
+              <td>
+                ₱{" "}
+                {cart.reduce((agg, i) => (agg += i.quantity * i.item.price), 0)}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div className="center">
+        <div className="contact-details">
+          <div className="info">
+            <span className="">Contact</span>
+            <span className="number">
+              <input
+                type="text"
+                disabled={disabledChangeContact}
+                value={mobileContact}
+              />
+            </span>
+            <span
+              onClick={() => setDisabledChangeContact(!disabledChangeContact)}
+              className="btn"
+            >
+              {disabledChangeContact ? "Change" : "Save"}
+            </span>
+          </div>
+          {deliveryMethod === "collect" && (
+            <div className="info">
+              <span className="">Pick-Up at</span>
+              <span>11 Emerald St. Severina Diamond, Paranaque City</span>
+              {/* <span className="btn">Change</span> */}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="buttons flex-end center">
+        <Link to="/checkout/information">Return to Information</Link>
+        <Button>Place Order</Button>
       </div>
     </div>
   );
