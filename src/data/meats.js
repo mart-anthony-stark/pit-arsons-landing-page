5817ae3b35a3059874f546f63c994e064b021b87
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-22 10:38:23 +0800

    fixed product descriptions

--- src/data/meats.js
+++ src/data/meats.js
@@ -5,7 +5,8 @@ export default [
     category: "Chicken",
     price: 350,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Chicken/halfsmoked-chicken.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Smoked cuts of chicken infused with peri-peri flavors (also comes in Whole chicken size).",
   },
   {
     id: 2,
@@ -13,7 +14,8 @@ export default [
     category: "Chicken",
     price: 600,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Chicken/whole%20smoked%20chicken.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Smoked cuts of chicken infused with peri-peri flavors (also comes in Half chicken size)",
   },
   {
     id: 3,
@@ -21,7 +23,8 @@ export default [
     category: "Pork",
     price: 1300,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Pork/pork%20ribs.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Slabs of pork ribs, smoked to perfection to deepen its intricate mixture of flavors.",
   },
   {
     id: 4,
@@ -29,7 +32,8 @@ export default [
     category: "Sausage",
     price: 380,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Sausage/sausage%204.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Vacuum packed beef sausages loaded with an invigorating smoky aroma. (Also comes in packs of 8)",
   },
   {
     id: 5,
@@ -37,7 +41,8 @@ export default [
     category: "Sausage",
     price: 750,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Sausage/sausage%208.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Vacuum packed beef sausages loaded with an invigorating smoky aroma. (Also comes in packs of 4)",
   },
   {
     id: 6,
@@ -45,23 +50,26 @@ export default [
     category: "Smoke U.S. Angus Beef",
     price: 500,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-AngusBeef/angus-250.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Authentic U.S. Angus flavors vacuum sealed in a slab of beef belly.(Also sold in 500g and 1kg packages)",
   },
   {
     id: 7,
     name: "500g USDA Angus Smoked Beef Belly",
     category: "Smoke U.S. Angus Beef",
-    price: 500,
+    price: 950,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-AngusBeef/angus-500.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Authentic U.S. Angus flavors vacuum sealed in a slab of beef belly.(Also sold in 250g and 1kg packages)",
   },
   {
     id: 8,
     name: "1kg USDA Angus Smoked Beef Belly",
     category: "Smoke U.S. Angus Beef",
-    price: 500,
+    price: 1900,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-AngusBeef/angus-1kg.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "Authentic U.S. Angus flavors vacuum sealed in a slab of beef belly.(Also sold in 250g and 500g packages)",
   },
   {
     id: 9,
@@ -69,7 +77,8 @@ export default [
     category: "Gift Box",
     price: 2950,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-GiftBox/token%20box.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "The perfect bundle for gift giving - appetize your friends and family now with this token box which includes 500g of USDA Angus Smoked beef Belly, Smoked Pork Ribs, 500g of Chilli Con Carne and one bottle of our signature BBQ Sauce.",
   },
   {
     id: 10,
@@ -77,7 +86,8 @@ export default [
     category: "Gift Box",
     price: 4300,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-GiftBox/black%20box.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "This bundle consists of 1kg of beef shortplate, 1 slab of baby back ribs, 1 pack of sausages, half peri-peri chicken and 3 bottles of barbecue sauce.",
   },
   {
     id: 11,
@@ -85,7 +95,8 @@ export default [
     category: "Others",
     price: 580,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Others/_salmon.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "This lovely pink fish is smoked to perfection, ready to melt in your mouth.",
   },
   {
     id: 12,
@@ -93,7 +104,8 @@ export default [
     category: "Others",
     price: 350,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Others/chili%20con%20carne.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "A classic mexican favorite that brings a protein punch to your diet - beef, beans, served with a side of nachos.",
   },
   {
     id: 13,
@@ -101,7 +113,8 @@ export default [
     category: "Others",
     price: 150,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Others/bbq%20sauce.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "A bottle of our signature barbecue sauce that enhances the flavor of our dishes. (Also comes in Extra Hot)",
   },
   {
     id: 14,
@@ -109,6 +122,7 @@ export default [
     category: "Others",
     price: 150,
     img: "https://github.com/mart-anthony-stark/pit-arsons-landing-page/blob/main/src/images/Meats-Others/extra%20hot%20bbq.jpg?raw=true",
-    description: "Slabs of pork ribs infused with an authentic smokey flavor.",
+    description:
+      "A spicy bottle of our signature barbecue sauce that enhances the flavor of our dishes. (Also comes in Original BBQ)",
   },
 ];
