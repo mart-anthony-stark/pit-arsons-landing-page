9d2c7febb8f4fcc11e9308dc00ccb497cfdc75b7
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-25 19:01:02 +0800

    Add:subscribe to newsletter

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,23 @@
 import "./style.css";
 import logo from "../../images/logo.png";
 import { Link } from "react-router-dom";
+import validator from "validator";
+import { useRef } from "react";
+import toast from "react-hot-toast";
 
 const Footer = () => {
+  const emailRef = useRef();
+  const handleSubscribeNewsletter = async () => {
+    let email = emailRef.current.value;
+    if (!validator.isEmail(email))
+      return toast.error("Invalid Email. Please try again");
+    await fetch(`${import.meta.env.VITE_API_BASE_URL}/email`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email }),
+    });
+    toast.success("You're now subscribed to our newsletter")
+  };
   return (
     <footer>
       <div className="footer-cont">
@@ -53,8 +68,8 @@ const Footer = () => {
         <div className="links newsletter">
           <h2>Newsletter</h2>
           <div className="email-input">
-            <input type="email" />
-            <button>Subscribe</button>
+            <input type="email" ref={emailRef} />
+            <button onClick={handleSubscribeNewsletter}>Subscribe</button>
           </div>
         </div>
       </div>
