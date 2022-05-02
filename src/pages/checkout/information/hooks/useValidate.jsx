362efe101fda745ebf7ed2ea685db0fff88bb3c0
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-02 14:41:18 +0800

    Add: required address validation

--- src/pages/checkout/information/hooks/useValidate.jsx
+++ src/pages/checkout/information/hooks/useValidate.jsx
@@ -5,12 +5,22 @@ import { useNavigate } from "react-router-dom";
 
 const useValidate = () => {
   const customer = useSelector((state) => state.information.customer);
+  const deliveryAddress = useSelector(
+    (state) => state.information.deliveryAddress
+  );
+
+  const deliveryDateTime = useSelector(
+    (state) => state.information.deliveryDateTime
+  );
   const navigate = useNavigate();
 
   const isValid = () => {
     let valid = true;
 
-    if (!validator.isEmail(customer.email)) {
+    if (
+      validator.isEmpty(customer.email) ||
+      !validator.isEmail(customer.email)
+    ) {
       valid = false;
       toast.error("Invalid Email");
     }
@@ -37,13 +47,29 @@ const useValidate = () => {
       toast.error("Mobile Number is required");
     }
 
+    if (validator.isEmpty(deliveryDateTime)) {
+      valid = false;
+      toast.error("Delivery date and time is required");
+    }
+    const { brgy, city, country, region, street, zip_code } = deliveryAddress;
+    if (
+      validator.isEmpty(brgy) ||
+      validator.isEmpty(city) ||
+      validator.isEmpty(country) ||
+      validator.isEmpty(region) ||
+      validator.isEmpty(street) ||
+      validator.isEmpty(zip_code)
+    ) {
+      valid = false;
+      toast.error("Delivery address is required");
+    }
+
     return valid;
   };
   const validate = () => {
-    if (isValid()) navigate("/checkout/payment");
+    return isValid();
   };
   return { validate };
 };
 
 export default useValidate;
-// "/checkout/payment"
